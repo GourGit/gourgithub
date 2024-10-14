8517aab2f04939f0531a6be43f9086ce000c4eb0
GourGit <basakgoursundar618@gmail.com>
2024-10-14 14:53:38 +0530

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,10 @@
+//Write a C program to calculate sum of two numbers
+#include <stdio.h> //Header file
+int main() //Main function
+{
+	int a,b;
+	printf("Enter two numbers: ");
+	scanf("%d%d",&a,&b);
+	printf("The sum is: %d",a+b);
+	return 0;
+}
