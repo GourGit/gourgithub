8d32cae21e654cd1096090a02b68d9f3605d7a11
GourGit <basakgoursundar618@gmail.com>
2025-01-18 22:31:40 +0530

    Update table practice.cpp

--- table practice.cpp
+++ table practice.cpp
@@ -2,7 +2,6 @@
 int main()
 {
 	int num,i;
-	//Take the nu32mber as an input from the user
 	printf("Enter the value of number whose multiplication table is to be printed\n");
 	scanf("%d",&num);
 	printf("The multiplication table of %d is\n",num);
