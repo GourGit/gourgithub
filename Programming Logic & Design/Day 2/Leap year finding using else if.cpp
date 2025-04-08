350e4236ffeaf8fc149ac5da3ba94e287a93c373
Gour Sundar Basak <basakgoursundar618@gmail.com>
2025-04-08 22:04:06 +0530

    Update Leap year finding using else if.cpp

--- Programming Logic & Design/Day 2/Leap year finding using else if.cpp
+++ Programming Logic & Design/Day 2/Leap year finding using else if.cpp
@@ -5,7 +5,7 @@ int main(){
 	scanf("%d",&year);
 	if(year%400==0)
 	printf("Leap year");
-	else if(year%100==0)
+	else if(year%100!=0)
 	printf("Not leap yaer");
 	else if(year%4==0)
 	printf("Leap year");
